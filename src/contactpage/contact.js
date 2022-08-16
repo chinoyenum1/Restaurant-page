3a0a73eca339ac7c5d64e50072a9226bc6c93839
Benaiah Enebeli <enebeli.benaiah@gmail.com>
2022-08-16 16:23:21 +0100

    Contact Page added

--- src/contactpage/contact.js
+++ src/contactpage/contact.js
@@ -1,3 +1,22 @@
+import {} from "../../styles/contact.css";
+import { content } from "../homepage/home";
+import { header } from "../homepage/utils/header";
+import { footer } from "../homepage/utils/footer";
+import { location } from "../contactpage/utils/location";
+import { form } from "../contactpage/utils/contact-form";
+
+
 export const contact = () => {
-    alert('Hello, i have a contact')
+    content.textContent = "";
+    const wrapper = document.createElement('div')
+    const page_title = document.createElement('h1')
+    page_title.textContent = `Contact Us`
+
+    wrapper.classList.add('wrapper')
+
+    wrapper.append(page_title, location(), form())
+
+
+  return content.append(header(), wrapper, footer());
+
 }
